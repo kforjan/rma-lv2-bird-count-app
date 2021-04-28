abcbe3c60ce04a06071a5f2dacd05283f4b999a9
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-04-28 14:47:33 +0200

    Basic UI added

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -10,6 +10,47 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bird Counter'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: Text(
+              '0',
+              style: TextStyle(fontSize: 60),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildIncrementButton('Brown', Colors.brown),
+              _buildIncrementButton('Grey', Colors.grey),
+              _buildIncrementButton('Black', Colors.black),
+              _buildIncrementButton('Orange', Colors.orange),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Reset'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIncrementButton(String text, Color color) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text(text),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(color),
+      ),
+    );
   }
 }
